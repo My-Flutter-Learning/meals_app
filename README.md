38767be036c587ec8122377420610be26ef078a1
victormutuku <victor.mutuku@strathmore.edu>
2022-03-24 16:27:02 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Meals App
+ This is my third Flutter project in which is a meals app callled 'DeliMeals'
---
+++ lib/category_item.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+
+class CategoryItem extends StatelessWidget {
+  final String title;
+  final Color color;
+
+  const CategoryItem(this.title, this.color, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.all(10),
+        child: Text(
+            title,
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              color.withOpacity(0.7),
+              color,
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter),
+            borderRadius: BorderRadius.circular(15),
+          ),
+      );
+  }
+}
