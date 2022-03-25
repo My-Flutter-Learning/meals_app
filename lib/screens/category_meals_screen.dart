9f5b5ace8c607c25716556cd10391e6aeba93cdc
victormutuku <victor.mutuku@strathmore.edu>
2022-03-25 15:54:27 +0300

    Passing data via a Constructor
    This allows us to change the title of the AppBar dynamically based on the selected category.

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
-  const CategoryMealsScreen({ Key? key }) : super(key: key);
+  final String categoryId;
+  final String categoryTitle;
+  const CategoryMealsScreen(this.categoryId, this.categoryTitle, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(title: const Text('The Recipes'),),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
       body: const Center(
         child: Text('The Recipes for this category!'),
       ),
