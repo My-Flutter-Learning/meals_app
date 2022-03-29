a497d057caf2b168382d57c6b6203c537aa54771
victormutuku <victor.mutuku@strathmore.edu>
2022-03-29 20:47:10 +0300

    Adding a bottom bar
    Switched the top tabs to bottom tabs and a dynamically changing appBar title

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -3,39 +3,44 @@ import 'package:meals_app/screens/categories_screen.dart';
 import 'package:meals_app/screens/favourites_screen.dart';
 
 class TabsScreen extends StatefulWidget {
-  const TabsScreen({ Key? key }) : super(key: key);
+  const TabsScreen({Key? key}) : super(key: key);
 
   @override
   State<TabsScreen> createState() => _TabsScreenState();
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, Object>> _pages = [
+    {'page': const CategoriesScreen(), 'title': 'Categories'},
+    {'page': const FavouritesScreen(), 'title': 'Your Favourite'}
+  ];
+
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      initialIndex: 0,
-      length: 2,
-      child: Scaffold(
-          appBar: AppBar(
-              title: const Text('Meals'),
-              bottom: const TabBar(
-                  tabs: [
-                      Tab(
-                          icon: Icon(Icons.category),
-                          text: 'Categories',
-                        ),
-                      Tab(
-                        icon: Icon(Icons.star),
-                        text: 'Favourites',
-                      ),
-                    ]
-                ),
-            ),
-          body: const TabBarView(children: [
-            CategoriesScreen(),
-            FavouritesScreen(),
+    return Scaffold(
+      appBar: AppBar(title:  Text(_pages[_selectedPageIndex]['title'] as String)),
+      body: _pages[_selectedPageIndex]['page'] as Widget,
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: _selectPage,
+          backgroundColor: Theme.of(context).primaryColor,
+          unselectedItemColor: Colors.white,
+          selectedItemColor: Theme.of(context).accentColor,
+          currentIndex: _selectedPageIndex,
+          // type: BottomNavigationBarType.fixed,
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.category), label: 'Categories'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.star), label: 'Favourites'),
           ]),
-        ),
     );
   }
 }
